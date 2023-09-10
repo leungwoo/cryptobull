c83fe7e2f1c4720a29ccf6b9e89272ff664900d7
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-09-10 00:17:30 -0400

    ...

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Theme appearance="dark">
+    <Theme>
       <App />
     </Theme>
   </React.StrictMode>
