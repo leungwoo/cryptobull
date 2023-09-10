0878dfb9c2f7436bacf577e055d56d70cef379c7
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-09-09 23:30:54 -0400

    fetched data for news section

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const News = () => {
-  return <div id="news">News</div>;
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log(news);
+
+  useEffect(() => {
+    const getNews = async () => {
+      const options = {
+        method: "GET",
+        url: "https://cryptocurrency-news2.p.rapidapi.com/v1/coindesk",
+        headers: {
+          "X-RapidAPI-Key":
+            "9ea0101f77msh5ed0ff4528c6926p15db84jsn0ee898e01cd6",
+          "X-RapidAPI-Host": "cryptocurrency-news2.p.rapidapi.com",
+        },
+      };
+      const includeWords = [
+        "bitcoin",
+        "cspr",
+        "xrp",
+        "altcoin",
+        "ethereum",
+        "xtp",
+        "qnt",
+      ];
+      try {
+        const response = await axios.request(options);
+        const apiData = response.data.data;
+        // Filter news items containing any of the includeWords
+        const filteredData = apiData.filter((item) =>
+          includeWords.some((word) =>
+            item.title.toLowerCase().includes(word.toLowerCase())
+          )
+        );
+        if (filteredData.length >= 8) {
+          setNews(filteredData.slice(0, 8));
+        } else {
+          setNews(filteredData);
+        }
+        setLoading(false); // Set loading to false
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getNews();
+  }, []);
+
+  return (
+    <div id="news" className="bg-gradient-to-tr from-tertiary to-pink">
+      {news.map((item, index) => {
+        return (
+          <div key={index}>
+            <div className="flex flex-col items-center justify-center">
+              <img src={item.thumbnail} alt="thumbnail" />
+              <h1 className="text-4xl font-bold text-center text-white">
+                {item.title}
+              </h1>
+              <p className="text-white">{item.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default News;
