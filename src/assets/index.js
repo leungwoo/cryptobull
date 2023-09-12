fab72af3414b72aa250ab454d6b4fc4e5ba9d099
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-09-11 21:08:42 -0400

    fix icons bg and hero size

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,10 @@
-import xrp from "./png-transparent-xrp-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment-icon-thumbnail.png";
-import bnb from "./png-transparent-bnb-binance-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon-thumbnail.png";
-import btc from "./png-transparent-btc-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment-icon-thumbnail.png";
-import ltc from "./png-transparent-litecoin-cryptocurrency-bitcoin-logo-bitcoin-logo-payment-bitcoin-thumbnail.png";
-import polygon from "./png-transparent-polygon-matic-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon-thumbnail.png";
-import ada from "./png-transparent-cardano-cryptocurrency-blockchain-initial-coin-offering-coin-white-text-rectangle-thumbnail.png";
-import eth from "./png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png";
-import xrp2 from "./png-transparent-xrp-cryptocurrency-logo-icon-thumbnail.png";
+import xrp from "./png-transparent-xrp-cryptocurrency-logo-icon-thumbnail-removebg-preview.png";
+import bnb from "./png-transparent-bnb-binance-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon-thumbnail-removebg-preview.png";
+import btc from "./png-transparent-btc-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment-icon-thumbnail-removebg-preview.png";
+import ltc from "./png-transparent-litecoin-cryptocurrency-bitcoin-logo-bitcoin-logo-payment-bitcoin-thumbnail-removebg-preview.png";
+import polygon from "./png-transparent-polygon-matic-coin-cryptocoin-exchange-coins-crypto-blockchain-cryptocurrency-logo-glyph-icon-thumbnail-removebg-preview.png";
+import ada from "./png-transparent-cardano-cryptocurrency-blockchain-initial-coin-offering-coin-white-text-rectangle-thumbnail-removebg-preview.png";
+import eth from "./png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail-removebg-preview.png";
+import xrp2 from "./png-transparent-xrp-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment-icon-thumbnail-removebg-preview.png";
 
 export { xrp, bnb, ltc, polygon, ada, eth, btc, xrp2 };
