ab49158e0087d0c282cde0a6d95d0c7ac0a63516
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-09-13 18:13:19 -0400

    fix menu icon

--- src/Components/DropDownMenu.jsx
+++ src/Components/DropDownMenu.jsx
@@ -6,17 +6,20 @@ const DropdownMenuDemo = () => {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
-        <button
-          className="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-violet11 bg-white shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black"
-          aria-label="Customise options"
-        >
-          <HamburgerMenuIcon color="#da058c" />
-        </button>
+        <div className="flex flex-row gap-2 items-center">
+          <p className="text-tertiary">Menu</p>
+          <button
+            className="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center bg-opacity-20 backdrop-blur-lg bg-pink   "
+            aria-label="Customise options"
+          >
+            <HamburgerMenuIcon color="#da058c" />
+          </button>
+        </div>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
         <DropdownMenu.Content
-          className="min-w-[220px] bg-white rounded-md p-[10px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+          className="min-w-[220px] bg-black rounded-md p-[10px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
           sideOffset={10}
         >
           <a href="#news">
