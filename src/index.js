6fd60703fa45829954c7189fd0751bc9853d2744
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-09-14 18:34:40 -0400

    delete radix theme

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+    <App />
   </React.StrictMode>
 );
