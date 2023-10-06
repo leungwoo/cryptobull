d8a09db43e48d89e2a3b28251921dc5514198508
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2023-10-06 09:00:13 -0400

    add a dark tint to hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,20 +8,16 @@ const Hero = () => {
       id="home"
       className="w-full h-[750px] bg-primary bg-no-repeat  bg-cover bg-left-top bg-slate-950 relative"
     >
+      <div className="absolute inset-0 bg-black opacity-60"></div>
       <Header />
       <div className="flex flex-col justify-end h-full">
-        {/* <img
-          className="absolute w-full h-[700px] object-cover"
-          alt="hero"
-          src="https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2048&q=80"
-        /> */}
         <div className="absolute flex flex-col gap-24 top-1/3  px-10  text-start xl:w-2/3">
           <div className="flex flex-col gap-5">
             <h3 className="text-base xl:text-xl font-medium text-tertiary">
               The Bulls are in charge
             </h3>
             <h1 className="font-bold sm:text-6xl leading-[50px] md:leading-[100px] text-4xl bg-gradient-to-r from-teal to-pink bg-clip-text text-transparent ">
-              Are your BAGS PACKED and READY?
+              Are you READY?
             </h1>
           </div>
           <a href="#toppicks">
